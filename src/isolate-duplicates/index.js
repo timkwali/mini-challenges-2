9b6967fba02a67e8891f58da3359b82416eeb313
Timring <timringperez@gmail.com>
2020-04-27 15:57:43 +0100

    turned string to array, obtain identical characters array

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,18 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+  if(typeof(text) !== "string") {
+    return "Please enter a valid string";
+  }
+
+  text = [...text];
+  let identicalCharacters = new Set(text);
+
+  for(let i = 0; i < text.length; i++) {
+    
+  }
+
+
+}
+
+isolateDuplicates("slfssfefls");
 
 module.exports = isolateDuplicates;
