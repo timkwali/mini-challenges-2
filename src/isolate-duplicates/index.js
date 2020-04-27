d16784f6dfddb00cb5f7e62f81a6e101f38d66ba
Timring <timringperez@gmail.com>
2020-04-27 19:18:21 +0100

    grouped letters according to type

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -5,14 +5,40 @@ function isolateDuplicates(text) {
 
   text = [...text];
   let identicalCharacters = new Set(text);
+  let charactersGrouping = [];
+
 
   for(let i = 0; i < text.length; i++) {
-    
+    //pick first character of a group for comparisons
+    console.log(i);
+    let currentLetter = text[i];
+    let currentLetterIndex = text.indexOf(currentLetter);
+    let group = [];
+
+    //compare first letter of group
+    for (let j = currentLetterIndex; j < text.length; j++) {
+      if(text[j] == currentLetter) {
+        group.push(text[i]);
+        //check for last character
+        if(j == text.length - 1) {
+          i = j;
+          charactersGrouping.push(group);
+          break;
+        }
+      } else {
+        i = j - 1;
+        charactersGrouping.push(group);
+        break;
+      }
+    }
+
   }
+  console.log(charactersGrouping)
+
 
 
 }
 
-isolateDuplicates("slfssfefls");
+isolateDuplicates("aaaabbcdefffffffg");
 
 module.exports = isolateDuplicates;
