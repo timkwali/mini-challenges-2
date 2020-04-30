d063aaa2913bc7f0a0a3652e96de6f461e98fcd6
Timring <timringperez@gmail.com>
2020-04-30 13:53:16 +0100

    sort object keys, rearrange object according to keys

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,34 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
 
-module.exports = removeDuplicates;
+  //remove duplicate elements of object values
+  for(let key in  obj) {
+    let ky = new Set(obj[key]);
+    obj[key] = ky;
+  }
+
+  //sort keys in ascending order
+  let keys = Object.keys(obj);
+  //keys = keys.map(key => parseInt(key));
+  keys = keys.sort((a, b) => a - b);
+
+  //arrange object properties according to the keys above
+  let object = {};
+  for(let i = 0; i < keys.length; i++) {
+    object[keys[i]] = obj[keys[i]];
+  }
+  obj = object;
+  console.log(obj)
+
+  //remove duplicates
+
+}
+
+removeDuplicates(
+  {
+    "2345": ["A", "B", "C", "Y", "D", "B"],
+    "45": ["A", "B", "D", "A"],
+    "325": ["A", "B", "T"],
+    "5": ["Q", "C"]
+  }
+);
+//module.exports = removeDuplicates;
